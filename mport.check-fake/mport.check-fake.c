83fbab4c1f6eefce7560f9125cb7a9d442e67a26
ctriv <ctriv@68ec6820-69e1-0310-87e8-ba08227f189b>
2008-04-04 19:50:30 +0000

    first working version.

--- mport.check-fake/mport.check-fake.c
+++ mport.check-fake/mport.check-fake.c
@@ -43,6 +43,12 @@ __MBSDID("$MidnightBSD: src/libexec/mport.delete/mport.delete.c,v 1.2 2008/01/05
 #include <sys/stat.h>
 #include <regex.h>
 
+#ifdef PRINT_DIAG
+#define DIAG(fmt, ...) warnx(fmt, ##__VA_ARGS__);
+#else
+#define DIAG(fmt, ...) 
+#endif
+
 static void usage(void);
 static int check_fake(mportPlist *, const char *, const char *, const char *);
 static int grep_file(const char *, const char *);
@@ -51,14 +57,23 @@ static int grep_file(const char *, const char *);
 int main(int argc, char *argv[]) 
 {
   int ch, ret;
-  const char *skip = NULL;
+  const char *skip = NULL, *prefix = NULL, *destdir = NULL, *plistfile = NULL;
   mportPlist *plist;
   FILE *fp;
-  
-  while ((ch = getopt(argc, argv, "s:")) != -1) {
+
+  while ((ch = getopt(argc, argv, "f:d:s:p:")) != -1) {
     switch (ch) {
       case 's':
         skip = optarg;
+        break;
+      case 'p':
+        prefix = optarg;
+        break;
+      case 'd':
+        destdir = optarg;
+        break;
+      case 'f':
+        plistfile = optarg;
         break;
       case '?':
       default:
@@ -70,15 +85,13 @@ int main(int argc, char *argv[])
   argc -= optind;
   argv += optind;
 
-  warnx("argc: %i", argc);
+  DIAG("plist = %s; destdir = %s; prefix = %s; skip = %s", plistfile, destdir, prefix, skip)
 
-  warnx("argv[0] = %s; argv[1] = %s; argv[2] = %s; argv[3] = %s", argv[0], argv[1], argv[2], argv[3]);
-
-  if (argc != 3) 
+  if (!prefix || !destdir || !plistfile) 
     usage();
   
-  if ((fp = fopen(argv[0], "r")) == NULL)
-    err(EX_NOINPUT, "Could not open plist file %s", argv[1]);
+  if ((fp = fopen(plistfile, "r")) == NULL)
+    err(EX_NOINPUT, "Could not open plist file %s", plistfile);
       
   if ((plist = mport_plist_new()) == NULL) 
     err(EX_OSERR, "Could not not allocate plist");
@@ -86,10 +99,10 @@ int main(int argc, char *argv[])
   if (mport_plist_parsefile(fp, plist) != MPORT_OK)
     err(EX_DATAERR, "Invalid plist");
 
-  warnx("running check_fake");  
+  DIAG("running check_fake")
   
-  printf("Checking %s\n", argv[1]);
-  ret = check_fake(plist, argv[1], argv[2], skip);
+  printf("Checking %s\n", destdir);
+  ret = check_fake(plist, destdir, prefix, skip);
   
   if (ret == 0) {
     printf("Fake succeeded.\n");
@@ -108,33 +121,37 @@ static int check_fake(mportPlist *plist, const char *destdir, const char *prefix
   char cwd[FILENAME_MAX], file[FILENAME_MAX];
   char *anchored_skip;
   struct stat st;
-  regex_t *skipre;
+  regex_t skipre;
   int ret = 0;
 
-  warnx("checking skip: %s", skip);
+  DIAG("checking skip: %s", skip)
     
   if (skip != NULL) {
-    warnx("Compiling skip: %s", skip);
+    DIAG("Compiling skip: %s", skip)
+    
     if (asprintf(&anchored_skip, "^%s$", skip) == -1)
       err(EX_OSERR, "Could not build skip regex");
-  
-    if (regcomp(skipre, skip, REG_EXTENDED|REG_NOSUB) != 0)
+    
+    if (regcomp(&skipre, anchored_skip, REG_EXTENDED|REG_NOSUB) != 0)
       errx(EX_DATAERR, "Could not compile skip regex");
+      
+     
+    free(anchored_skip);  
   }
   
-  warnx("Coping prefix (%s) to cwd", prefix);
+  DIAG("Coping prefix (%s) to cwd", prefix)
   
   (void)strlcpy(cwd, prefix, FILENAME_MAX);
 
-  warnx("Starting loop, cwd: %s", cwd);
+  DIAG("Starting loop, cwd: %s", cwd)
   
   STAILQ_FOREACH(e, plist, next) {
     if (e->type == PLIST_CWD) {
         if (e->data == NULL) {
-          warnx("Setting cwd to '%s'", prefix);
+          DIAG("Setting cwd to '%s'", prefix)
           (void)strlcpy(cwd, prefix, FILENAME_MAX);
         } else {
-          warnx("Setting cwd to '%s'", e->data);
+          DIAG("Setting cwd to '%s'", e->data)
           (void)strlcpy(cwd, e->data, FILENAME_MAX);
         }
         
@@ -146,7 +163,7 @@ static int check_fake(mportPlist *plist, const char *destdir, const char *prefix
     
     (void)snprintf(file, FILENAME_MAX, "%s%s/%s", destdir, cwd, e->data);
 
-    warnx("checking %s", file);
+    DIAG("checking %s", file)
       
     if (lstat(file, &st) != 0) {
       (void)snprintf(file, FILENAME_MAX, "%s/%s", cwd, e->data);
@@ -166,32 +183,77 @@ static int check_fake(mportPlist *plist, const char *destdir, const char *prefix
 
 
     /* if file matches skip continue */
-    if (skip != NULL && (regexec(skipre, e->data, 0, NULL, 0) == 0))
+    if (skip != NULL && (regexec(&skipre, e->data, 0, NULL, 0) == 0))
       continue;      
     
+    
+    DIAG("==> Grepping %s", file)
     /* grep file for fake destdir */
     if (grep_file(file, destdir)) {
-      (void)printf("    %s contains the fake destdir", e->data);
+      (void)printf("    %s contains the fake destdir\n", e->data);
       ret = 1;
     }
   }
   
   if (skip != NULL)
-    regfree(skipre);
+    regfree(&skipre);
   
   return ret;
 } 
       
 
-static int grep_file(const char *file, const char *destdir)
+static int grep_file(const char *filename, const char *destdir)
 {
-  if (file || destdir)
-    return 0;
+  FILE *file;
+  char *line, *nline;
+  static regex_t regex;
+  static int compiled = 0;
+  size_t len;
+  int ret = 0;
+  
+  DIAG("===> Compiling destdir: %s", destdir)
+  
+  /* Should we cache the compiled regex? */
+  if (!compiled) {
+    if (regcomp(&regex, destdir, REG_EXTENDED|REG_NOSUB) != 0)
+      errx(EX_DATAERR, "Could not compile destdir regex");
+    compiled++;
+  }
+  
+  if ((file = fopen(filename, "r")) == NULL)
+    err(EX_SOFTWARE, "Couldn't open %s", filename);
+    
+  while ((line = fgetln(file, &len)) != NULL) {
+    /* if we end in \n just switch it to \0, otherwise we need more mem */
+    if (line[len - 1] == '\n') {
+      line[len - 1] = '\0';
+    } else {
+      nline = (char *)malloc((len + 1) * sizeof(char));
+      if (nline == NULL)
+        err(EX_OSERR, "Couldn't allocate nline buf");
+      
+      memcpy(nline, line, len);
+      nline[len] = '\0';
+      nline = line;
+    }
+    
+    if (regexec(&regex, line, 0, NULL, 0) == 0) {
+      DIAG("===> Match line: %s", line);
+      ret = 1;      
+      break;
+    }
+  }
+  
+  if (ferror(file) != 0)
+    err(EX_IOERR, "Error reading %s", filename);
+  
+  fclose(file);
+  return ret;
 }
       
 static void usage() 
 {
-  errx(EX_USAGE, "Usage: mport.delete [-s skip] plist destdir prefix");
+  errx(EX_USAGE, "Usage: mport.delete [-s skip] <-f plist> <-d destdir> <-p prefix>");
   exit(EX_USAGE);
 }
 
